553bf22ab48ab479b131ad084ff57940c27a4255
unciafidelis <unciafidelis@gmail.com>
2022-04-22 19:15:53 -0500

    Add: Updated! message in postman

--- app.js
+++ app.js
@@ -34,4 +34,10 @@ app.post('/v1/explorers', (req, res) => {
     res.status(201).json({message: "Created"})
 })
 
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body //Client parameters
+    res.status(200).json({message: "Updated!"})
+})
 
