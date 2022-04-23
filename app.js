0a2536bf7fc38b4b1c84b626823ff4994533649c
unciafidelis <unciafidelis@gmail.com>
2022-04-22 19:10:31 -0500

    Add: Message "created" json in postman

--- app.js
+++ app.js
@@ -28,3 +28,10 @@ app.get('/v1/explorers/:id', (req, res) => {
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //Client parameters
+    res.status(201).json({message: "Created"})
+})
+
+
